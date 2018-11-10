95d983032d69f14643c274e79bd9bffcffb1ace7
wky623 <1009904664@qq.com>
2018-11-10 18:07:02 +0800

    PlayerAct1
    movement and attack

---
+++ Immortal/Assets/Scripts/Test.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+    Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(transform.forward.normalized * 1000);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+    
+    }
+}
