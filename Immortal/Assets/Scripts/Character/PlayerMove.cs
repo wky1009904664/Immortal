dc5e18592cf748bebf251382e93d76f18cb69d3e
Joker <303359872@qq.com>
2018-12-13 19:22:14 +0800

    tower1

---
+++ Immortal/Assets/Scripts/Character/PlayerMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour {
+
+    private Animator anim;
+    public float speed = 20;
+    public float rotateSpeed = 30;
+
+    // Use this for initialization
+    void Start()
+    {
+        anim = this.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //控制移动
+        float h = Input.GetAxis("Horizontal");  //水平的  左和右
+        float v = Input.GetAxis("Vertical");    //竖直的  前和后
+
+        transform.Translate(new Vector3(0, 0, v) * speed * Time.deltaTime);
+        if(h!=0)
+            transform.Rotate(Vector3.up * h*rotateSpeed * Time.deltaTime);
+
+        //  GetComponent<Rigidbody>().MovePosition(transform.position + new Vector3(h, 0, v) * speed*Time.deltaTime);
+
+    }
+}
---
+++ Immortal/Assets/Scripts/SwapCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwapCamera : MonoBehaviour {
+
+    private GameObject mainCamera;
+    private GameObject camera2;
+    public SetMirrorCP mirrorPlane;
+    public int k;
+    public static SwapCamera Instance;
+
+	// Use this for initialization
+	void Start () {
+        Instance = this;
+        k = 0;
+        mainCamera = GameObject.Find("Camera0");
+        camera2 = GameObject.Find("Camera1");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            k = 1 - k;
+            if (k > 0)
+            {
+                mainCamera.SetActive(false);
+                camera2.SetActive(true);
+            } else
+            {
+                mainCamera.SetActive(true);
+                camera2.SetActive(false);
+            }
+        }
+    }
+}
