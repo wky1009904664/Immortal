e1e3b935137fc31b1e1348c2990727dbdc4d9e04
Joker <303359872@qq.com>
2018-12-17 14:07:52 +0800

    Immortal1.2

---
+++ Immortal/Assets/Scripts/RandomThings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomThings : MonoBehaviour {
+
+    public Transform[] pos;
+    public GameObject zhu;
+    private int a, b;
+
+	// Use this for initialization
+	void Start () {
+        a = Random.Range(0, pos.Length);
+        b = Random.Range(0, pos.Length);
+        while (a == b) b = Random.Range(0, pos.Length);
+        GameObject.Instantiate(zhu, pos[a].position,Quaternion.identity);
+        GameObject.Instantiate(zhu, pos[b].position,Quaternion.identity);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
